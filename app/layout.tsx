d2a625a991e86f11de7d2af0925c29858e23886c
Nsbhadawar <97339127+bhadawar@users.noreply.github.com>
2025-11-29 23:13:03 +0530

    homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <link rel="icon" href="/favicon.svg" /> */}
-      <title>Loyalty Credit</title>
+      <title>loyalty credit</title>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeRegistry>
           {showHeader ? <Layout>{children}</Layout> : <>{children}</>}
